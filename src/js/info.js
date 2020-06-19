a9f563af05d25f96a2b35455171c8e5a0ebd962b
ed_e <edwoill@gmail.com>
2020-06-19 14:06:06 +0100

    Adding featureRestrictPermissionLevel sysop bypass

--- src/js/info.js
+++ src/js/info.js
@@ -151,6 +151,7 @@ rw.config = `+ JSON.stringify(rw.config) +"; //</nowiki>"; // generate config te
         // Restrict feature to users in this group
         mw.user.getGroups(g=>{
             let hasPerm = g.includes(l);
+            hasPerm = g.includes("sysop"); // admins override all feature restrictions
             if ((l == "confirmed") && !hasPerm) {hasPerm = g.includes("autoconfirmed");} // Due to 2 types of confirmed user, confirmed and autoconfirmed, we have to check both
             if (hasPerm) {
                 // Has the permission needed

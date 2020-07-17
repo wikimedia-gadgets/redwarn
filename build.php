a431b2a9b8d2dab9b50aaf98efcadf384240a1ec
Chlod Alejandro <chlod@chlod.net>
2020-07-17 17:37:45 +0000

    Build script improvements

--- build.php
+++ build.php
@@ -1,13 +1,18 @@
 <?php
 // RedWarn's very basic build tool
 // (c) Ed E
-// USAGE: run in terminal or w a request
-// to save to file php build.php > redwarn,js
+//
+// USAGE: Run this in the terminal, or grab it as a web request.
+//
+// To output the built script into a file, run the follwing:
+//
+//     php build.php > redwarn.js
+//
 
-function buildScript() {
-    // For each js file
-    $result = "";
-    $jsFiles = [
+// ==== Part 1. Defining constants ====
+
+$jsRoot = __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "js";
+$jsFiles = [
     'styles.js',
     'init.js',
     'dialog.js',
@@ -23,29 +28,35 @@ function buildScript() {
     'quickTemplate.js',
     'pageProtect.js',
     'han.js'
-    ]; // List of files in order to import 
-    foreach($jsFiles as $file) {
-        $result .= file_get_contents("./src/js/" . $file) . "\n"; // get contents and append
-    }
+];
 
-    $files = glob('./src/html/*.html', GLOB_BRACE); // for each html file
-    foreach($files as $file) {
-        $result = str_replace("[[[[include ". end(explode("/", $file)). "]]]]", file_get_contents($file), $result); // replace include statements
-      
-        // Generate and add build info
-        $result = str_replace("[[[[BUILDINFO]]]]", '
-Build Time: '. date('d/m/Y H:i:s', time()) .' UTC
-Excecuted script: '. str_replace("\\", "/", __FILE__) .'
-User: '. get_current_user() . '@'. gethostname() .' on '. php_uname('s') .'
-', $result);
-    }
+$htmlRoot = __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "html";
 
-    return '
-/*
+$magicWords = [
+	"BUILDINFO" =>
+		"Build Time: " . date('d/m/Y H:i:s', time()) . "UTC" . PHP_EOL .
+        "Excecuted script: " . str_replace("\\", "/", __FILE__) . PHP_EOL .
+        "User: " . get_current_user() . '@'. gethostname() .' on '. php_uname('s')
+];
+
+// ==== Part 2. Function definitions ====
+
+/**
+ * Output the credits for RedWarn.
+ **/
+function getCredits() {
+?>
 R E D W A R N
 (c) 2020 Ed. E and contributors - ed6767wiki (at) gmail.com
 Licensed under the Apache License 2.0 - read more at https://gitlab.com/redwarn/redwarn-web/
+<?php
+}
 
+/**
+ * Output the notices for Wikipedia editors.
+ **/
+function getNotice() {
+?>
 +-------------------------------------------+
 |                                           |
 | ATTENTION ALL USERS WITH SCRIPT CHANGE    |
@@ -61,21 +72,115 @@ Licensed under the Apache License 2.0 - read more at https://gitlab.com/redwarn/
 +-------------------------------------------+
 
 To all normal users, if you wish to customise RedWarn, submit a request on the talk page or download source.
-*/
-// <nowiki>
-'. $result . '
-    $( document ).ready( function () {
-        // Init when page loaded
-      try {
-        initRW();
-      } catch (err) {
-        mw.notify("Sorry, an error occured while loading RedWarn.");
-        console.error(err);
-      }
-    } );
-// </nowiki>
-    ';
+<?php
 }
 
-header("content-type: application/javascript");
-print buildScript();
+/**
+ * Read a JavaScript file from the sources directory.
+ *
+ * @param $file string The filename to be used
+ * @return false|string The contents of the requested JavaScript file.
+ */
+function readJSSourceFile($file) {
+    global $jsRoot;
+
+    return file_get_contents($jsRoot . DIRECTORY_SEPARATOR . $file);
+}
+
+/**
+ * Process JavaScript includes and magic words.
+ *
+ * @param $fileContents string Raw executable JavaScript.
+ * @return string The processed JavaScript code.
+ **/
+function processJS($fileContents) {
+    return processMagicWords(
+		processIncludedFiles(
+			$fileContents
+		)
+	);
+}
+
+/**
+ * Process JavaScript includes.
+ *
+ * @param $fileContents string The JavaScript code to be processed.
+ * @return string The processed JavaScript code.
+ **/
+function processIncludedFiles($fileContents) {
+    return preg_replace_callback(
+		"#\[\[\[\[include (.+?)]]]]#",
+		function ($matches) {
+			global $htmlRoot;
+
+			$filePath = $htmlRoot . DIRECTORY_SEPARATOR . $matches[1];
+
+			return file_exists($filePath) ?
+				file_get_contents($filePath) : "[[[[FAILED INCLUDE (" . $matches[1]. ")]]]]";
+		},
+        $fileContents
+	);
+}
+
+/**
+ * Process magic words into their respective output values.
+ *
+ * @param $fileContents string The JavaScript code to be processed.
+ * @return string The processed JavaScript code.
+ **/
+function processMagicWords($fileContents) {
+	return preg_replace_callback(
+		"#\[\[\[\[(\S+)]]]]#",
+		function ($matches) {
+			global $magicWords;
+			if (isset($magicWords[$matches[1]]))
+				return $magicWords[$matches[1]];
+			else
+				return $matches[0];
+		},
+        $fileContents
+	);
+}
+
+/**
+ * Outputs processed JavaScript from the loading list.
+ **/
+function getJSSources() {
+    global $jsFiles;
+    $js = "";
+
+    foreach($jsFiles as $file) {
+        $js .= readJSSourceFile($file) . PHP_EOL;
+    }
+
+    echo processJS($js);
+}
+
+/**
+ * Outputs everything.
+ */
+function buildScript() {?>
+/*
+<?php getCredits(); ?>
+
+<?php getNotice(); ?>
+*/
+// <nowiki>
+<?php getJSSources(); ?>
+$( document ).ready( function () {
+	// Initialize RedWarn once the page is loaded.
+	try {
+		initRW();
+	} catch (err) {
+		mw.notify("Sorry, an error occured while loading RedWarn.");
+		console.error(err);
+	}
+});
+// </nowiki>
+<?php
+}
+
+// ==== Part 3. Output ====
+
+header("Content-Type: application/javascript; charset=utf-8");
+buildScript();

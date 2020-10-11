5dad27eadd244ae7438b98c821302b346ccbe88e
ChlodAlejandro <chlodaidanalejandro@hotmail.com>
2020-10-11 16:15:25 +0800

    redirect rename

---
+++ src/util/redirect.tsx
@@ -0,0 +1,14 @@
+import {h as TSX} from "tsx-dom";
+
+/**
+ * Redirects a user to another page.
+ * @param url The new page's URL
+ * @param inNewTab Whether to open the page in a new tab or not.
+ */
+export default function(url: string, inNewTab = false): void {
+    if (inNewTab) {
+        (<a target="_blank" href={url}/>).click(); // Open in new tab
+    } else {
+        window.location.href = url; // open here
+    }
+}

10044137245187b15b328131fc430acff72c4f53
sportshead <32637656+sportshead@users.noreply.github.com>
2020-11-14 12:35:22 +0800

    Rollback work - not done yet

--- src/wikipedia/Rollback.tsx
+++ src/wikipedia/Rollback.tsx
@@ -1,7 +1,80 @@
-import { h as TSX } from "tsx-dom";
+import { BaseProps, h as TSX } from "tsx-dom";
+import RedWarnStore from "../data/RedWarnStore";
+import redirect from "../util/redirect";
+import WikipediaAPI from "./API";
 
 export default class Rollback {
-    static async loadIcons() {
+    static getRollbackRevId(): string {
+        const isNLatest = $("#mw-diff-ntitle1")
+            .text()
+            .includes("Latest revision");
+        const isOLatest = $("#mw-diff-otitle1")
+            .text()
+            .includes("Latest revision");
+        if (isNLatest) {
+            // Return the revID of the edit on the right
+            return $("#mw-diff-ntitle1 > strong > a")
+                .attr("href")
+                .split("&")[1]
+                .split("=")[1];
+        } else if (isOLatest) {
+            return $("#mw-diff-otitle1 > strong > a")
+                .attr("href")
+                .split("&")[1]
+                .split("=")[1];
+        } else {
+            // BUG!
+            // TODO dialog
+            /* rw.ui.confirmDialog(
+                "A very weird error occurred. (rollback getRollbackRevID failed via final else!)",
+                "REPORT BUG",
+                () =>
+                    rw.ui.reportBug(
+                        "rollback getRollbackRevID failed via final else! related URL: " +
+                            window.location.href
+                    ),
+                "",
+                () => {},
+                0
+            ); */
+        }
+    }
+
+    static async preview(): Promise<void> {
+        // TODO dialog
+        //rw.ui.loadDialog.show("Loading preview...");
+        // Check if latest, else redirect
+        /* rw.info.isLatestRevision(mw.config.get("wgRelevantPageName"), rw.rollback.getRollbackrevID(), un=>{
+            // Fetch latest revision not by user
+            rw.info.latestRevisionNotByUser(mw.config.get("wgRelevantPageName"), un, (content, summary, rID) => {
+                // Got it! Now open preview dialog
+
+                // Add handler for when page loaded
+                let url = rw.wikiIndex + "?title="+ mw.config.get("wgRelevantPageName") +"&diff="+ rID +"&oldid="+ mw.util.getParamValue("diff") +"&diffmode=source#rollbackPreview";
+                redirect(url); // goto in current tab
+            });
+        }); */
+        const un = await WikipediaAPI.isLatestRevision(
+            mw.config.get("wgRelevantPageName"),
+            this.getRollbackRevId()
+        );
+        const { revid } = await WikipediaAPI.latestRevisionNotByUser(
+            mw.config.get("wgRelevantPageName"),
+            un
+        );
+        const url =
+            RedWarnStore.wikiIndex +
+            "?title=" +
+            mw.config.get("wgRelevantPageName") +
+            "&diff=" +
+            revid +
+            "&oldid=" +
+            mw.util.getParamValue("diff") +
+            "&diffmode=source#rollbackPreview";
+        redirect(url);
+    }
+
+    static async loadIcons(): Promise<void> {
         // Check if page is editable, if not, don't show
         if (!mw.config.get("wgIsProbablyEditable")) {
             // Can't edit, so exit
@@ -17,9 +90,11 @@ export default class Rollback {
             .text()
             .includes("Latest revision"); // is the left side the latest revision? (rev13 bug fix)
 
-        const currentRevIcons = <span id="rwCurrentRevRollbackBtns"></span> as HTMLSpanElement;
+        let currentRevIcons = (
+            <span id="rwCurrentRevRollbackBtns"></span>
+        ) as HTMLSpanElement;
 
-        RollbackIcons.forEach((icon, i)=>{
+        RollbackIcons.forEach((icon, i) => {
             const id = `rwRollback_${i}`;
             let clickHandler;
 
@@ -27,27 +102,264 @@ export default class Rollback {
                 clickHandler = icon.action;
             } else {
                 if (!icon.promptReason) {
-                    clickHandler = () => this.rollback(icon.summary, null, icon.ruleIndex);
+                    clickHandler = () =>
+                        this.rollback(icon.summary, icon.ruleIndex);
                 } else {
-                    clickHandler = () => this.promptRollbackReason(icon.summary);
+                    clickHandler = () =>
+                        this.promptRollbackReason(icon.summary);
                 }
             }
 
             if (icon.enabled) {
-                const elem = <div id={id} class="icon material-icons">
-                    <span style={{
-                        cursor: "pointer",
-                        fontSize: "28px",
-                        paddingRight: "5px",
-                        color: icon.color
-                    }} onClick={clickHandler}></span>
-                </div>;
-                const iconElem = <div class="mdl-tooltip mdl-tooltip--large" for={id}>
-                    {icon.name}
-                </div>;
+                const elem = (
+                    <div id={id} class="icon material-icons">
+                        <span
+                            style={{
+                                cursor: "pointer",
+                                fontSize: "28px",
+                                paddingRight: "5px",
+                                color: icon.color,
+                            }}
+                            onClick={clickHandler}
+                        ></span>
+                    </div>
+                );
+                const iconElem = (
+                    <div class="mdl-tooltip mdl-tooltip--large" for={id}>
+                        {icon.name}
+                    </div>
+                );
                 currentRevIcons.appendChild(elem);
                 currentRevIcons.appendChild(iconElem);
             }
+        });
+
+        currentRevIcons = (
+            <div>
+                {currentRevIcons}
+                <span id="rwRollbackInProgress" style="display:none;">
+                    <div
+                        id="rwRollbackInProgressBar"
+                        class="mdl-progress mdl-js-progress"
+                        style="width:300px; display: block; margin-left: auto; margin-right: auto;"
+                    ></div>
+                    <div style="height:5px"></div>
+                    {/* <!-- spacer --> */}
+                    <span style="font-family: Roboto;font-size: 16px;">
+                        Reverting...
+                    </span>
+                    <br />
+                    <div style="height:5px"></div>
+                    {/* <!-- spacer --> */}
+                </span>
+                {/* <!-- Rollback complete icons --> */}
+                <span id="rwRollbackDoneIcons" style="display:none;">
+                    {/* <!-- Done indicator --> */}
+                    <div style="height:5px"></div>
+                    {/* <!-- spacer --> */}
+                    <span style="font-family: Roboto;font-size: 16px;display: inline-flex;vertical-align: middle;">
+                        <span
+                            class="material-icons"
+                            style="color:green;cursor:default;"
+                        >
+                            check_circle
+                        </span>
+                        &nbsp;&nbsp; Rollback complete
+                    </span>
+                    <br />
+                    <div style="height:5px"></div>
+                    {/* <!-- spacer --> */}
+                    {/* <!-- CONTROL ICONS - todo add onclick events --> */}
+                    <div id="RWRBDONEmrevPg" class="icon material-icons">
+                        <span style="cursor: pointer;">watch_later</span>
+                    </div>
+                    <div
+                        class="mdl-tooltip mdl-tooltip--large"
+                        for="RWRBDONEmrevPg"
+                    >
+                        Go to latest revision
+                    </div>
+                    &nbsp;&nbsp;
+                    <div id="RWRBDONEnewUsrMsg" class="icon material-icons">
+                        <span style="cursor: pointer;">send</span>
+                    </div>
+                    <div
+                        class="mdl-tooltip mdl-tooltip--large"
+                        for="RWRBDONEnewUsrMsg"
+                    >
+                        New Message
+                    </div>
+                    &nbsp;&nbsp;
+                    <div id="RWRBDONEwelcomeUsr" class="icon material-icons">
+                        <span style="cursor: pointer;">library_add</span>
+                    </div>
+                    <div
+                        class="mdl-tooltip mdl-tooltip--large"
+                        for="RWRBDONEwelcomeUsr"
+                    >
+                        Quick Template
+                    </div>
+                    &nbsp;&nbsp;
+                    <div id="RWRBDONEwarnUsr" class="icon material-icons">
+                        <span style="cursor: pointer;">report</span>
+                    </div>
+                    <div
+                        class="mdl-tooltip mdl-tooltip--large"
+                        for="RWRBDONEwarnUsr"
+                    >
+                        Warn User
+                    </div>
+                    &nbsp;&nbsp;
+                    <div id="RWRBDONEreportUsr" class="icon material-icons">
+                        <span style="cursor: pointer;">gavel</span>
+                    </div>
+                    <div
+                        class="mdl-tooltip mdl-tooltip--large"
+                        for="RWRBDONEreportUsr"
+                    >
+                        Report to Admin
+                    </div>
+                </span>
+            </div>
+        );
+
+        // idk why eslint decides to die for me
+        // eslint-disable-next-line quotes
+        const twinkleLoadedBeforeUs = $('div[id^="tw-revert"]').length > 0;
+
+        const left = isLeftLatest ? (
+            currentRevIcons
+        ) : (
+            <RestoreElement left={true} />
+        );
+        if (twinkleLoadedBeforeUs) {
+            // eslint-disable-next-line quotes
+            $('.diff-otitle > div[id^="tw-revert"]').after(left);
+        } else {
+            $(".diff-otitle").prepend(left);
+        }
+
+        const right = isLatest ? (
+            currentRevIcons
+        ) : (
+            <RestoreElement left={false} />
+        ); // if the latest rev, show the accurate revs, else, don't
+        if (twinkleLoadedBeforeUs) {
+            // eslint-disable-next-line quotes
+            $('.diff-ntitle > div[id^="tw-revert"]').after(right);
+        } else {
+            $(".diff-ntitle").prepend(right);
+        }
+
+        // TODO visuals
+        /* setTimeout(()=>{
+            // Register all tooltips after 50ms (just some processing time)
+            for (const item of document.getElementsByClassName("mdl-tooltip")) {
+                rw.visuals.register(item);
+            }
+
+            // Register progressbar
+            rw.visuals.register($("#rwRollbackInProgressBar")[0]);
+        },100); */
+    }
+    static async rollback(
+        reason: string,
+        defaultWarnIndex: number
+    ): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    static getDisabledHTMLandHandlers(): HTMLElement[] {
+        // Open a new dialog with all the disabled icons so user can select one. Click handlers are already registered, so we just call rw.rollback.clickHandlers.[elID]();
+        // Load Rollback current rev icons (rev14)
+        const finalIconStr = [];
+        RollbackIcons.forEach((icon, i) => {
+            if (icon.enabled) return; // if icon is enabled, we can skip
+            if (icon.name == "More Options") return; // does nothing here, so not needed
+
+            const elID = "rwRollback_" + i; // get the ID for the new icons
+
+            // Establish element with all the info
+            finalIconStr.push(
+                <div>
+                    <div
+                        class="mdl-button mdl-js-button"
+                        style="width:100%; text-align: left;"
+                        onclick={`window.parent.postMessage('rwRollbackBtn${elID}', '*');`}
+                    >
+                        <span
+                            class="material-icons"
+                            style={`padding-right:20px;color:${icon.color};`}
+                        >
+                            {icon.icon}
+                        </span>
+                        <span
+                            style={
+                                icon.name.length > 40 ? "font-size: 12px;" : ""
+                            }
+                        >
+                            {icon.name}
+                        </span>
+                    </div>
+                    <hr style="margin:0" />
+                </div>
+            );
+
+            // Add click event handler
+            // TODO addMessageHandler dialog
+            /* addMessageHandler("rwRollbackBtn" + elID, () => {
+                dialogEngine.closeDialog(() => {
+                    // close dialog, then
+                    rw.rollback.clickHandlers[elID](); // send our callback
+                });
+            }); */
+        });
+
+        // Now return HTML (rw16)
+        return finalIconStr;
+    }
+
+    static showRollbackDoneOps(un: string, warnIndex: number) {
+        // Clear get hidden handler to stop errors in more options menu
+        rw.rollback.getDisabledHTMLandHandlers = () => {
+            return "";
+        }; // return w empty string
+        // Add click handlers
+        $("#RWRBDONEmrevPg").click(() =>
+            rw.info.isLatestRevision(
+                mw.config.get("wgRelevantPageName"),
+                0,
+                () => {}
+            )
+        ); // go to latest revision
+        $("#RWRBDONEnewUsrMsg").click(() => rw.ui.newMsg(un)); // send message
+        $("#RWRBDONEwelcomeUsr").click(() =>
+            rw.quickTemplate.openSelectPack(un)
+        ); // quick template
+        $("#RWRBDONEwarnUsr").click(() =>
+            rw.ui.beginWarn(
+                false,
+                un,
+                mw.config.get("wgRelevantPageName"),
+                null,
+                null,
+                null,
+                warnIndex != null ? warnIndex : null
+            )
+        ); // Warn User
+        $("#RWRBDONEreportUsr").click(() => rw.ui.adminReportSelector(un)); // report to admin
+
+        // Now perform default (if set)
+        if (
+            rw.config.rwRollbackDoneOption != null ||
+            rw.config.rwRollbackDoneOption != "none"
+        )
+            $(`#${rw.config.rwRollbackDoneOption}`).click();
+
+        // Hides other icons and shows the rollback done options and also checks for defaults, also adds click handlers
+        $("#rwRollbackInProgress").fadeOut(() => {
+            // fade out - looks smoother
+            $("#rwRollbackDoneIcons").fadeIn(); //show our icons
         });
     }
 }
@@ -71,6 +383,45 @@ interface RollbackIconBase {
     name: string;
     color: string;
     icon: string;
+}
+
+interface RestoreProps extends BaseProps {
+    left: boolean;
+}
+
+function RestoreElement(props: RestoreProps) {
+    return (
+        <div>
+            <div
+                id={`rOld${props.left ? "1" : "2"}`}
+                class="icon material-icons"
+            >
+                <span
+                    style="cursor: pointer; font-size:28px; padding-right:5px; color:purple;"
+                    onClick={() => {
+                        Rollback.promptRestoreReason(
+                            $(
+                                `#mw-diff-${
+                                    props.left ? "o" : "n"
+                                }title1 > strong > a`
+                            )
+                                .attr("href")
+                                .split("&")[1]
+                                .split("=")[1]
+                        );
+                    }}
+                >
+                    history
+                </span>
+            </div>
+            <div
+                class="mdl-tooltip mdl-tooltip--large"
+                for={`rOld${props.left ? "1" : "2"}`}
+            >
+                Restore this version
+            </div>
+        </div>
+    );
 }
 
 export type RollbackIcon = RollbackIconBase & RollbackAction;

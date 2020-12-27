d1106eeed7f0a1c34c47964a435b454e092c754c
sportshead <7168668-sportshead@users.noreply.gitlab.com>
2020-12-27 14:53:09 +0800

    ok, lets try this again (optimization)

--- src/styles/material/hooks/MaterialPreInitializationHooks.ts
+++ src/styles/material/hooks/MaterialPreInitializationHooks.ts
@@ -1,9 +1,7 @@
 import RedWarnStore from "../../../data/RedWarnStore";
 
-import mdc from "material-components-web";
 import { MaterialStyleStorage } from "../storage/MaterialStyleStorage";
 
 export default function (): void {
     RedWarnStore.styleStorage = new MaterialStyleStorage();
-    mdc.autoInit();
 }

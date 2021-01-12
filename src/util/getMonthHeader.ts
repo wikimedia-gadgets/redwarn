051c8fadf6664378720f171b373625bbadea1b07
sportshead <7168668-sportshead@users.noreply.gitlab.com>
2021-01-12 15:59:49 +0800

    optimization: use mw.config.get("wgMonthNames") instead of an array

--- src/util/getMonthHeader.ts
+++ src/util/getMonthHeader.ts
@@ -1,22 +1,9 @@
-export const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-];
-
 /**
  * Gets the month header for talk pages in the format `Month YYYY`.
  * @param date The date to use.
  */
 export default function (date = new Date()): string {
-    return `${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+    return `${
+        mw.config.get("wgMonthNames")[date.getMonth() + 1]
+    } ${date.getFullYear()}`;
 }

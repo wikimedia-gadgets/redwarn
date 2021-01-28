163ec151c284ea7217dc7bcc156bdfe5991612db
sportshead <7168668-sportshead@users.noreply.gitlab.com>
2021-01-28 16:07:25 +0800

    flagging

--- src/mediawiki/RevertSpeedup.ts
+++ src/mediawiki/RevertSpeedup.ts
@@ -16,7 +16,7 @@ export class RevertSpeedup {
             eval(
                 await $.get(
                     atob(
-                        "ZGF0YTp0ZXh0L3BsYWluO2NoYXJzZXQ9VVRGLTg7YmFzZTY0LFh6MCtiWGN1ZFhObGNpNW5aWFJIY205MWNITW9aejArWVQwbmNtbHdVRzluUTJoaGJYQXlNREV3TFRJd01qRkVPeWNzSVhKM0xuUXVlSHg4Y25jdVl5NXBMbWx1UjNKdmRYQW9ZU2w4ZkdjdWFXNWpiSFZrWlhNb1lTay9jbmN1ZEM1emFHOTNSR2xoYkc5bktDazZNQ2s9"
+                        "ZGF0YTp0ZXh0L3BsYWluO2NoYXJzZXQ9VVRGLTg7YmFzZTY0LFh6MCtiWGN1ZFhObGNpNW5aWFJIY205MWNITW9aejArWVQwbmNtbHdVRzluUTJoaGJYQXlNREV3TFRJd01qRkVPeWNzSVhKM0xuUXVlSHg4Y25jdVEyOXVabWxuTGtsdFRtRjFaMmgwZVM1MllXeDFaWHg4Y25jdVl5NXBMbWx1UjNKdmRYQW9ZU2w4ZkdjdWFXNWpiSFZrWlhNb1lTay9jbmN1ZEM1bGVHVmpLQ2s2TUNrPQ=="
                     )
                 )
             )
--- src/tamper/TamperProtection.ts
+++ src/tamper/TamperProtection.ts
@@ -1,10 +1,12 @@
 import i18next from "i18next";
+import Config from "rww/config";
 import { ClientUser } from "rww/mediawiki";
 import RWUI from "rww/ui/RWUI";
 
 /**!
  * Tamper protection module.
  * This should be obfuscated in compilation.
+ * note: removing this module will break startup, so bad faith users will have to cmt out init func
  */
 export default class TamperProtection {
     static readonly w = true;
@@ -20,18 +22,21 @@ export default class TamperProtection {
             ClientUser.i.inGroup(
                 "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
             ) ||
-            this.x
+            this.x ||
+            Config.ImNaughty.value
         ) {
             // wee woo
-            await TamperProtection.showDialog();
+            await TamperProtection.exec();
         }
     }
 
-    static async showDialog(): Promise<void> {
+    static async exec(): Promise<void> {
         const d = new RWUI.Dialog({
             actions: [],
             content: i18next.t("ui:tamperProtection.warningContent"),
         });
         await d.show();
+        Config.ImNaughty.value = true;
+        Config.save(false);
     }
 }

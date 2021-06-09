a83f76e85a1bd01a34095bc1eb94c1881103db73
ed_e <edwoill@gmail.com>
2021-06-09 21:58:55 +0100

    update dev version format

--- src/data/RedWarnConstants.ts
+++ src/data/RedWarnConstants.ts
@@ -9,7 +9,7 @@ export const RW_VERSION = "0.2.0";
 export const RW_VERSION_PUBLIC = "17";
 export const RW_VERSION_TAG = `${RW_VERSION_PUBLIC}${
     process.env.NODE_ENV === "development"
-        ? `dev+${RW_BUILDINFO.gitHashShort}`
+        ? `dev (${RW_BUILDINFO.gitHashShort})`
         : ""
 }`;
 export const RW_VERSION_SUMMARY = "nothing yet";

5bc6f2a6a37d42d769342c4a9a63d7d1762c5122
ChlodAlejandro <chlod@chlod.net>
2021-07-19 10:59:54 +0800

    switch to MutationObserver for tooltips

--- src/styles/material/hooks/MaterialPreInitializationHooks.tsx
+++ src/styles/material/hooks/MaterialPreInitializationHooks.tsx
@@ -4,38 +4,50 @@ import { MaterialStyleStorage } from "rww/styles/material/data/MaterialStyleStor
 import { MDCTooltip } from "@material/tooltip";
 import { h } from "tsx-dom";
 import { generateId } from "rww/util";
+import Log from "rww/data/RedWarnLog";
 
 export default function (): void {
     RedWarnStore.styleStorage = new MaterialStyleStorage();
 
-    // MDC Tooltip upgrade
-    document.addEventListener("animationstart", (event) => {
-        // Not even an element.
-        if (!(event.target instanceof Element)) return;
-        // Does not have the tooltip attribute.
-        if (!event.target.hasAttribute("data-rw-mdc-tooltip")) return;
-        // Tooltip already created.
-        if (event.target.classList.contains("rw-mdc-tooltip__upgraded")) return;
+    Log.debug("Starting MutationObserver...");
+    new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            document
+                .querySelectorAll(
+                    "[data-rw-mdc-tooltip]:not(.rw-mdc-tooltip__upgraded)"
+                )
+                .forEach((element) => {
+                    const id = `icobtn__${generateId(8)}`;
 
-        const id = `icobtn__${generateId(8)}`;
+                    const tooltipElement = (
+                        <div
+                            id={id}
+                            class="mdc-tooltip"
+                            role="tooltip"
+                            aria-hidden="true"
+                        >
+                            <div class="mdc-tooltip__surface">
+                                {element.getAttribute("data-rw-mdc-tooltip")}
+                            </div>
+                        </div>
+                    );
 
-        const tooltipElement = (
-            <div id={id} class="mdc-tooltip" role="tooltip" aria-hidden="true">
-                <div class="mdc-tooltip__surface">
-                    {event.target.getAttribute("data-rw-mdc-tooltip")}
-                </div>
-            </div>
-        );
+                    element.setAttribute("data-tooltip-id", id);
+                    element.setAttribute("aria-describedby", id);
 
-        event.target.setAttribute("data-tooltip-id", id);
-        event.target.setAttribute("aria-describedby", id);
+                    element.classList.add("rw-mdc-tooltip__upgraded");
 
-        event.target.classList.add("rw-mdc-tooltip__upgraded");
+                    element.parentElement.insertBefore(tooltipElement, element);
 
-        event.target.parentElement.insertBefore(tooltipElement, event.target);
-
-        new MDCTooltip(tooltipElement);
+                    new MDCTooltip(tooltipElement);
+                    Log.trace("Upgraded tooltip.", tooltipElement);
+                });
+        }
+    }).observe(document.body, {
+        childList: true,
+        subtree: true,
     });
+    Log.debug("MutationObserver stared.");
 
     // Periodic tooltip cleanup. Slightly expensive, so make the delay a bit big.
     setInterval(() => {

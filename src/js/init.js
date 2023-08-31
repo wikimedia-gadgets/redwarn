15f8c9318bc1ae65359e993a066e9d5a819c6193
Chlod Alejandro <chlod@chlod.net>
2023-08-31 18:54:02 +0800

    isolate usage of jquery.ui

--- src/js/init.js
+++ src/js/init.js
@@ -222,8 +222,9 @@ var rw = {
 
             // Load script files
             // Load MDL and everything needed, then callback when all loaded
-            rw.visuals.contextMenuPromise = mw.loader.getScript("https://redwarn.toolforge.org/cdn/js/jquery-contextmenu.js");
-            mw.loader.getScript("https://redwarn.toolforge.org/cdn/js/jquery-ui-position.js");
+            rw.visuals.contextMenuPromise = mw.loader.using(["jquery.ui"], () => {
+                return mw.loader.getScript("https://redwarn.toolforge.org/cdn/js/jquery-contextmenu.js");
+            });
             // Dialog polyfill for Firefox browsers
             mw.loader.getScript("https://redwarn.toolforge.org/cdn/js/dialogPolyfill.js");
             // Material Design Lite
